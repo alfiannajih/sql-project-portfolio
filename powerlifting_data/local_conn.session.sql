a7ad2eacda2d3c2066b364acf5c60dd185ef5a64
Alfian <alfiannajih@gmail.com>
2023-04-20 06:58:50 +0700

    validate bodyweightkg

--- powerlifting_data/local_conn.session.sql
+++ powerlifting_data/local_conn.session.sql
@@ -1,8 +1,16 @@
-SELECT
-    name,
-    best3squatkg,
-    best3benchkg,
-    best3deadliftkg,
-    totalkg
-FROM powerlift_data
-LIMIT 20
+WITH
+age_quartile AS (
+    SELECT
+        name,
+        age,
+        NTILE(4) OVER (ORDER BY age) AS quartile
+    FROM powerlift_data
+),
+quart_1_3 AS(
+    SELECT
+        quartile,
+        MAX(age) AS age
+    FROM age_quartile
+    WHERE quartile IN (1, 3)
+    GROUP BY quartile
+)
