2bf75a65153fbbff0de957a850aeb5d3ee978144
Alfian <alfiannajih@gmail.com>
2023-04-20 07:01:49 +0700

    query bodyweightkg

--- powerlifting_data/local_conn.session.sql
+++ powerlifting_data/local_conn.session.sql
@@ -1,16 +1,3 @@
-WITH
-age_quartile AS (
-    SELECT
-        name,
-        age,
-        NTILE(4) OVER (ORDER BY age) AS quartile
-    FROM powerlift_data
-),
-quart_1_3 AS(
-    SELECT
-        quartile,
-        MAX(age) AS age
-    FROM age_quartile
-    WHERE quartile IN (1, 3)
-    GROUP BY quartile
-)
+SELECT bodyweightkg
+FROM powerlift_data
+LIMIT 50;
