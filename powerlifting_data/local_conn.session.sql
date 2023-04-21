c391b38e8c58c4f8872cd50864eb4146b8fce33b
Alfian <alfiannajih@gmail.com>
2023-04-21 13:08:14 +0700

    rename table and create backup

--- powerlifting_data/local_conn.session.sql
+++ powerlifting_data/local_conn.session.sql
@@ -1,6 +1,5 @@
-SELECT
-    name, 
-    bodyweightkg
-FROM powerlift_data
-ORDER BY bodyweightkg
-LIMIT 10
+ALTER TABLE powerlift_data
+RENAME TO powerlift_backup;
+
+ALTER TABLE with_temp_id
+RENAME TO powerlift_data;
