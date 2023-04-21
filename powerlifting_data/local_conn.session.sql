82e062aa880b02a690842ebb08945cb8e6d39c8a
Alfian <alfiannajih@gmail.com>
2023-04-21 21:02:09 +0700

    query event percentage

--- powerlifting_data/local_conn.session.sql
+++ powerlifting_data/local_conn.session.sql
@@ -1,5 +1,14 @@
-ALTER TABLE powerlift_data
-RENAME TO powerlift_backup;
+WITH
+unique_event AS (
+    SELECT
+        event,
+        COUNT(*) AS count_event
+    FROM powerlift_data
+    GROUP BY event
+)
 
-ALTER TABLE with_temp_id
-RENAME TO powerlift_data;
+SELECT
+    event,
+    ROUND(count_event / SUM(count_event) OVER() * 100, 3) AS percentage
+FROM unique_event
+ORDER BY percentage DESC;
