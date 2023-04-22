8398cafef6fcfd7ac4899642f030c6526d49dba9
Alfian <alfiannajih@gmail.com>
2023-04-22 17:13:49 +0700

    query successful lift in SBD

--- powerlifting_data/local_conn.session.sql
+++ powerlifting_data/local_conn.session.sql
@@ -1,14 +1,22 @@
-WITH
-unique_event AS (
+WITH 
+squat_lift AS (
     SELECT
-        event,
-        COUNT(*) AS count_event
+        squat1kg,
+        squat2kg,
+        squat3kg
     FROM powerlift_data
-    GROUP BY event
+    WHERE
+        event = 'SBD'
+        AND
+        squat1kg IS NOT NULL
+        AND
+        squat2kg IS NOT NULL
+        AND
+        squat3kg IS NOT NULL
 )
 
 SELECT
-    event,
-    ROUND(count_event / SUM(count_event) OVER() * 100, 3) AS percentage
-FROM unique_event
-ORDER BY percentage DESC;
+    ROUND((SELECT COUNT(squat1kg) FROM squat_lift WHERE squat1kg > 0)::NUMERIC / COUNT(squat1kg) * 100, 2) AS squat_1_precentage,
+    ROUND((SELECT COUNT(squat2kg) FROM squat_lift WHERE squat2kg > 0)::NUMERIC / COUNT(squat2kg) * 100, 2) AS squat_2_precentage,
+    ROUND((SELECT COUNT(squat3kg) FROM squat_lift WHERE squat3kg > 0)::NUMERIC / COUNT(squat3kg) * 100, 2) AS squat_3_percentage
+FROM squat_lift
