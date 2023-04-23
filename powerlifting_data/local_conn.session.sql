54adfb5ed2173c4b044160c6991452f78029e957
Alfian <alfiannajih@gmail.com>
2023-04-23 13:56:15 +0700

    query average best lift

--- powerlifting_data/local_conn.session.sql
+++ powerlifting_data/local_conn.session.sql
@@ -1,22 +1,13 @@
-WITH 
-squat_lift AS (
-    SELECT
-        squat1kg,
-        squat2kg,
-        squat3kg
-    FROM powerlift_data
-    WHERE
-        event = 'SBD'
-        AND
-        squat1kg IS NOT NULL
-        AND
-        squat2kg IS NOT NULL
-        AND
-        squat3kg IS NOT NULL
-)
-
 SELECT
-    ROUND((SELECT COUNT(squat1kg) FROM squat_lift WHERE squat1kg > 0)::NUMERIC / COUNT(squat1kg) * 100, 2) AS squat_1_precentage,
-    ROUND((SELECT COUNT(squat2kg) FROM squat_lift WHERE squat2kg > 0)::NUMERIC / COUNT(squat2kg) * 100, 2) AS squat_2_precentage,
-    ROUND((SELECT COUNT(squat3kg) FROM squat_lift WHERE squat3kg > 0)::NUMERIC / COUNT(squat3kg) * 100, 2) AS squat_3_percentage
-FROM squat_lift
+    CONCAT(MIN(DATE_PART('year', date))::VARCHAR, '-', MAX(DATE_PART('year', date))::VARCHAR) AS year_range,
+    sex,
+    ROUND(AVG(best3deadliftkg), 2) AS average_deadlift
+FROM powerlift_data
+WHERE
+    best3deadliftkg > 0
+GROUP BY
+    FLOOR(DATE_PART('year', date)/5),
+    sex
+ORDER BY
+    year_range,
+    sex
