878a8aff88b33d706074024a3851aa94a1aef404
Alfian <alfiannajih@gmail.com>
2023-04-24 12:07:33 +0700

    export plot best average python script

---
+++ powerlifting_data/plot_best_average.py
@@ -0,0 +1,40 @@
+from matplotlib import pyplot as plt
+import pandas as pd
+import os
+
+def save_plot(csv_data, title):
+    df = pd.read_csv(csv_data)
+
+    for sex in df['sex'].unique():
+        temp_df = df[df['sex'] == sex]
+
+        plt.figure(figsize=(8, 4))
+        
+        plt.plot(
+            temp_df['year_range'],
+            temp_df.iloc[:, -1]
+        )
+
+        plt.xticks(rotation=30)
+        plt.ylim(temp_df.iloc[:, -1].min() * 0.9, temp_df.iloc[:, -1].max() * 1.1)
+
+        plt.title(f'{title}_{sex}')
+        plt.xlabel('Year Range')
+        plt.ylabel('Weight (kg)')
+
+        plt.savefig(f'plot_graph/{title}_{sex}.png', bbox_inches='tight')
+
+        plt.close()
+
+        print(f'{title}_{sex}.png Saved!')
+
+list_file = os.listdir('query_csv')
+
+print(f'Found {len(list_file)} csv files')
+
+for file in list_file:
+    csv_file = 'query_csv/' + file
+
+    save_plot(csv_file, file.replace('.csv', ''))
+
+print('Successfully Saved!')
