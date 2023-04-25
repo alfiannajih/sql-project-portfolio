ec4d5be19a6d53f9227e8e2650f8d02dc7ef5474
Alfian <alfiannajih@gmail.com>
2023-04-25 20:11:23 +0700

    change variance to standard deviation

--- powerlifting_data/local_conn.session.sql
+++ powerlifting_data/local_conn.session.sql
@@ -1,3 +1,10 @@
-SELECT *
+SELECT
+    age_range,
+    total_powerlifter,
+    mean_bw,
+    median_bw,
+    mode_bw,
+    std_bw,
+    range_bw
 FROM desc_stat_age
-WHERE sex = 'Mx';
+WHERE sex = 'Mx'
